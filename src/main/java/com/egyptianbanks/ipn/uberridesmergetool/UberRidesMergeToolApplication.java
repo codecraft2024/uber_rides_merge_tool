f6d632634f2807921e0b21bc22bec56df442b56d
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2025-05-03 20:25:27 +0300

    init

---
+++ src/main/java/com/egyptianbanks/ipn/uberridesmergetool/UberRidesMergeToolApplication.java
@@ -0,0 +1,13 @@
+package com.egyptianbanks.ipn.uberridesmergetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberRidesMergeToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberRidesMergeToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egyptianbanks/ipn/uberridesmergetool/UberRidesMergeToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egyptianbanks.ipn.uberridesmergetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberRidesMergeToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
