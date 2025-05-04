0c81c51cc5bd7f192154876cfc0823ee55a351f6
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2025-05-04 19:17:18 +0300

    add icon -adjust setup

---
+++ src/main/resources/launch.bat
@@ -0,0 +1,4 @@
+@echo off
+title Uber Rides Merge Tool
+java -jar uber-rides-merge-tool.jar
+pause
--- src/test/java/com/egyptianbanks/ipn/uberridesmergetool/UberRidesMergeToolApplicationTests.java
+++ src/test/java/com/egyptianbanks/ipn/uberridesmergetool/UberRidesMergeToolApplicationTests.java
@@ -3,11 +3,3 @@ package com.egyptianbanks.ipn.uberridesmergetool;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UberRidesMergeToolApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
